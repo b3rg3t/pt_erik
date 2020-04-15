2f6dc647b3c0df628b9336b5fc7778edba0a1b66
b3rg3t <berget_90db@hotmail.com>
2020-04-15 17:02:08 +0200

    typescript update

--- components/CardBlockFolder/CardBlock.tsx
+++ components/CardBlockFolder/CardBlock.tsx
@@ -4,43 +4,60 @@ import { useAmp } from "next/amp";
 import { AMPurl } from "../../helpers/helpdata";
 import CardBlockStyling from "./CardBlockStyling";
 
-const CardBlock = (props: any): React.ReactElement => {
+interface CardBlockProps {
+  allCards: [{
+    id: string;
+    name: string;
+    routepage: string;
+    linkpage: {
+      id: string;
+      title: string;
+    }
+    smallpic: {
+      alt: string;
+      height: string;
+      width: string;
+      url: string;
+    }
+    description: string;
+  }]
+}
+
+const CardBlock = ({ allCards }: CardBlockProps): React.ReactElement => {
   const isAmp = useAmp();
-  const cardData = props.allCards;
-  console.log(cardData);
+  const cardData = allCards;
   return (
-    cardData && (
+    cardData.length > 0 && (
       <>
         <div className="cards">
           <div className="cards__block">
-            {cardData.length > 0 &&
-              cardData.map((card, index) => {
-                const id = card?.linkpage?.id;
-                const href = `/${card.routepage[0]}${id ? `/[id]` : ""}${isAmp ? AMPurl.url : ""}`
-                const as = `/${card.routepage[0] + `${id ? `/${id}` : ""}` + `${isAmp ? AMPurl.url : ""}`}`
-                return (
-                  <article key={index} className="cards__panels" style={{
-                    backgroundImage: card.smallpic.url &&
-                      `url(${card.smallpic.url})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center center"
+            {cardData.map((card, index) => {
+              const id = card?.linkpage?.id;
+              const href = `/${card.routepage[0]}${id ? `/[id]` : ""}${isAmp ? AMPurl.url : ""}`
+              const as = `/${card.routepage[0] + `${id ? `/${id}` : ""}` + `${isAmp ? AMPurl.url : ""}`}`
+              return (
+                <article key={index} className="cards__panels" style={{
+                  backgroundImage: card.smallpic.url &&
+                    `url(${card.smallpic.url})`,
+                  backgroundSize: "cover",
+                  backgroundPosition: "center center"
+                }}>
+                  <div className="cards__panels__content" style={{
+                    background: `rgb(0,0,0,0.75)`,
                   }}>
-                    <div className="cards__panels__content" style={{
-                      background: `rgb(0,0,0,0.75)`,
-                    }}>
-                      <div className="header"><h3 className="h-tag">{card.name.toUpperCase()}</h3></div>
-                      <div className="description">
-                        <p style={{ color: "white" }}>{card.description.substring(0, 180)}</p>
-                      </div>
-                      <div className="button__div">
-                        <Link href={href} as={as} >
-                          <a title={card.routepage[0]} className="card-btn">Läs mer</a>
-                        </Link>
-                      </div>
+                    <div className="header"><h3 className="h-tag">{card.name.toUpperCase()}</h3></div>
+                    <div className="description">
+                      <p style={{ color: "white" }}>{card.description.substring(0, 180)}</p>
                     </div>
-                  </article>
-                )
-              })}
+                    <div className="button__div">
+                      <Link href={href} as={as} >
+                        <a title={card.routepage[0]} className="card-btn">Läs mer</a>
+                      </Link>
+                    </div>
+                  </div>
+                </article>
+              )
+            })}
           </div>
         </div>
         <CardBlockStyling />
